86c31257bb83ec512a359b0e127e45a414d4a3a0
Eaz Rumel <eazrumel@Eazs-MacBook-Air.local>
2026-07-29 22:35:34 +0600

    updated the manager orders by seller with axios and tanstack

--- src/layouts/Dashboard/Seller/ManageOrders.jsx
+++ src/layouts/Dashboard/Seller/ManageOrders.jsx
@@ -1,26 +1,69 @@
 import { useQuery } from "@tanstack/react-query";
 import userAxiosSecure from "../../../hooks/userAxiosSecure";
 import useAuth from "../../../hooks/useAuth";
+import MyInventoryRow from "./MyInventoryRow";
+import ManageOrdersRow from "./ManageOrdersRow";
 
 
 
 const ManageOrders = () => {
   const {user} = useAuth();
 
-  const axiosSecure= userAxiosSecure();
+  const axiosSecure = userAxiosSecure();
 
   const {data:orders=[],isLoading,refetch} = useQuery({
     queryKey:["orders",user?.email],
     queryFn:async()=>{
       const {data} = await axiosSecure.get(`/seller-order/${user.email}`)
-      return data
+      return data;
     }
     
   })
+
+
+  
   console.log("Orders from client side: ",orders)
+
+
+   
   return (
-    <div>
-      
+     <div className="mx-auto my-10">
+<div className="flex justify-evenly my-5">
+
+     <h1 className="h1 text-center  text-lime-400">Manage Users</h1>
+</div>
+
+  <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+
+       
+        <th>Name</th>
+       
+        <th>Customer Email</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Address</th>
+       <th>Status</th>
+       <th>Update</th>
+       <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+
+{/* mapping thorough each object */}
+       {
+        orders.map(orderData=>
+         <ManageOrdersRow orderData={orderData} key={orderData._id} refetch={refetch} ></ManageOrdersRow>
+        )
+       }
+
+    </tbody>
+    
+  </table>
+</div>
     </div>
   );
 };
---
+++ src/layouts/Dashboard/Seller/ManageOrdersRow.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import DeleteInventoryModal from './DeleteInventoryModal';
+import { notyf } from '../../../api/utils';
+import userAxiosSecure from '../../../hooks/userAxiosSecure';
+
+const ManageOrdersRow = ({orderData,refetch}) => {
+  const axiosSecure = userAxiosSecure();
+  const {customer,name,price,address,quantity,status,_id} = orderData;
+
+  let[isOpen,setIsOpen] = useState(false);
+
+  function open(){
+    setIsOpen(true)
+  }
+
+  function closeModal(){
+    setIsOpen(false)
+  }
+
+  const handleDelete = async()=>{
+    try {
+      const {result} = await axiosSecure.delete(`/order/${_id}`)
+      console.log(result)
+      notyf.success("Order deleted")
+      refetch();
+    
+
+    }
+    catch(error){
+      notyf.error(error.response.data)
+      console.log(error)
+      console.log(error.message)
+    }
+
+    finally{
+      closeModal();
+    }
+   }
+  return (
+      <tr>
+      <td>
+        {name}
+      </td>
+      <td>{customer?.email}</td>
+
+      <td>
+   
+       {price}
+      </td>
+     
+
+      <td>
+          {quantity}
+      </td>
+      <td>
+        {address}
+      </td>
+
+      <td>
+           {
+            status
+           }
+   
+     </td>
+     <td>
+        <div className="space-y-1">
+  <select
+
+    name="update"
+
+    id="update"
+
+    defaultValue={status}
+
+    className="w-full px-2 py-1 border border-green-300 focus:outline-green-300 rounded-md"
+
+  >
+
+    <option value="BDT">Pending</option>
+
+    <option value="USD">Start Processing</option>
+
+    <option value="EUR">Delivered</option>
+
+  </select>
+
+</div>
+     </td>
+     <td>
+      <button onClick={()=>setIsOpen(true)} className="bg-red-500 px-2 py-1 rounded-full text-red-300 cursor-pointer">Cancel</button>
+     </td>
+     <DeleteInventoryModal handleDelete={handleDelete} isOpen={isOpen} closeModal={closeModal}></DeleteInventoryModal>
+  
+    </tr>
+  );
+};
+
+export default ManageOrdersRow;
